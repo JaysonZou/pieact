be22212f2740d403e3c12defd9e4a21097508a2c
zjy960812 <1120957260@qq.com>
2023-09-27 17:52:54 +0800

    add useState hook

--- index.js
+++ index.js
@@ -200,3 +200,25 @@ function reconcileChildren(wipFiber, elements) {
     index++;
   }
 }
+
+let workInProgressFiber;
+let hookIndex;
+
+function useState(initial) {}
+
+function updateFunctionComponent(fiber) {
+  workInProgressFiber = fiber;
+  hookIndex = 0;
+  workInProgressFiber.hooks = [];
+  const children = [fiber.type(fiber.props)];
+  reconcileChildren(fiber, children);
+}
+
+/** @jsx Didact.createElement */
+function Counter() {
+  const [state, setState] = Didact.useState(1);
+  return <h1 onClick={() => setState((c) => c + 1)}>Count: {state}</h1>;
+}
+const element = <Counter />;
+const container = document.getElementById("root");
+Didact.render(element, container);
